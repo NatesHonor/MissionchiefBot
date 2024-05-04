9f2539e769216888a73e51318f6f3592f3da927d
NatesHonor <nathaniel.j.marcellus@gmail.com>
2024-05-03 22:02:44 -0400

    Updated mapping

--- data/personnel_mapping.py
+++ data/personnel_mapping.py
@@ -2,4 +2,5 @@ personnel_map = {
     "SWAT Personnel (In SWAT Vehicles)": "SWAT Armoured Vehicle",
     "Traffic Control": "Police Traffic Blocker Unit",
     "EMS Mobile Command": "EMS Mobile Command Unit",
+    "USAR": "Fire Traffic Blocker",
 }
