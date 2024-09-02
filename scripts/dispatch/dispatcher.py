3524bb0faad7c6634580ac91af09a60c1999a595
NatesHonor <nathaniel.j.marcellus@gmail.com>
2024-09-02 19:38:54 -0400

    Updated Dispatching logic

--- scripts/dispatch/dispatcher.py
+++ scripts/dispatch/dispatcher.py
@@ -5,7 +5,7 @@ import math
 from scripts.dispatch.vehicles.tow_truck import dispatch_tow_truck
 from scripts.dispatch.vehicles.prisoner_transport import dispatch_police_transport
 from scripts.dispatch.personnel.personnel import dispatch_personnel
-from scripts.dispatch.functions.select import select_vehicle
+from scripts.dispatch.functions.select import select_vehicles
 from scripts.dispatch.functions.check import transport_needed, prisoner_needed, wait_for_element, \
     click_and_wait_for_refresh
 from selenium.webdriver.common.by import By
@@ -18,13 +18,14 @@ logging.basicConfig(level=logging.INFO)
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-
 def dispatch_vehicles(driver, mission_id, vehicle_pool, mission_requirements,
                       vehicle_dispatch_mapping, crashed_cars, mission_data_file, prisoners, personnel_dispatch_mapping):
     mission_url = f"https://www.missionchief.com/missions/{mission_id}"
     driver.get(mission_url)
-    click_and_wait_for_refresh(driver, '.btn.btn-xs.btn-warning.missing_vehicles_load.btn-block')
+    try:
+        click_and_wait_for_refresh(driver, '.btn.btn-xs.btn-warning.missing_vehicles_load.btn-block')
+    except TimeoutException:
+        logging.error("Timeout while waiting for 'Load missing' button.")
 
     transport_needed(driver, mission_id)
     prisoner_needed(driver, mission_id)
@@ -45,10 +46,10 @@ def dispatch_vehicles(driver, mission_id, vehicle_pool, mission_requirements,
             logging.info(f"Dispatching Mass Casualty Unit instead of 7 ambulances for mission {mission_id}.")
             vehicle_type_names = vehicle_dispatch_mapping.get("mass casualty unit")
             if vehicle_type_names:
-                if select_vehicle(driver, vehicle_type_names, "mass casualty unit"):
-                    required_count -= 7
-                    if required_count <= 0:
-                        continue
+                dispatched_count = select_vehicles(driver, vehicle_type_names, "mass casualty unit", required_count)
+                required_count -= dispatched_count
+                if required_count <= 0:
+                    continue
 
         if (requirement == "k-9 unit" or requirement == "k-9 unit") and required_count > 2:
             logging.info(f"Dispatching K-9 Carrier instead of K-9 Unit for mission {mission_id}.")
@@ -76,19 +77,13 @@ def dispatch_vehicles(driver, mission_id, vehicle_pool, mission_requirements,
         else:
             vehicle_types = [vehicle_type_names]
         for vehicle_type in vehicle_types:
-            dispatched_count = 0
             matching_vehicles = {vehicle_id: info for vehicle_id, info in vehicle_pool.copy().items() if
                                  info['name'] == vehicle_type}
 
-            for vehicle_id, vehicle_info in matching_vehicles.items():
-                if dispatched_count < required_count:
-                    if select_vehicle(driver, vehicle_id, vehicle_type):
-                        dispatched_count += 1
-                        if dispatched_count == required_count:
-                            break
-            else:
-                logging.info(f"Dispatched {dispatched_count} out of {required_count} for {vehicle_type}")
-            if dispatched_count == required_count:
+            vehicle_ids = list(matching_vehicles.keys())
+            dispatched_count = select_vehicles(driver, vehicle_ids, vehicle_type, required_count)
+            logging.info(f"Dispatched {dispatched_count} out of {required_count} for {vehicle_type}")
+            if dispatched_count >= required_count:
                 break
 
     try:
