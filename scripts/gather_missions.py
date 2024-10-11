d75ce8ebbbe5728111bc1408343d09e2f2cb1712
Nate Hernandez-Marcellus <nathaniel.marellus@outlook.com>
2024-10-11 13:48:11 -0400

    Updated gather_missions.py

--- scripts/gather_missions.py
+++ scripts/gather_missions.py
@@ -1,19 +1,34 @@
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def total_number_of_missions(driver):
     driver.get("https://www.missionchief.com/")
-    time.sleep(1)
-    css_selector = 'div.mission_panel_red'
-    div_elements = driver.find_elements(By.CSS_SELECTOR, css_selector)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'missions')))
+    try:
+        mission_list = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mission_list')))
+    except Exception as e:
+        print("Error: Mission list not found")
+        return []
+
+    div_elements = mission_list.find_elements(By.CSS_SELECTOR, 'div.mission_panel_red')
     print(f"# of Missions found: {len(div_elements)}")
 
-    time.sleep(3)
     mission_numbers = []
     for div in div_elements:
-        href = div.find_element(By.TAG_NAME, 'a').get_attribute("href")
-        mission_number = href.split("/")[-1].split("?")[0]
-        mission_numbers.append(mission_number)
-        print(f"All found Mission Numbers: {mission_number}")
+        try:
+            mission_link = div.find_element(By.TAG_NAME, 'a')
+            mission_caption = mission_link.text.strip()
+            if "[Alliance]" in mission_caption:
+                continue
+
+            href = mission_link.get_attribute("href")
+            mission_number = href.split("/")[-1].split("?")[0]
+            mission_numbers.append(mission_number)
+            print(f"Found Mission Number: {mission_number}")
+        except Exception as e:
+            print(f"Error processing div: {e}")
+
     return mission_numbers
