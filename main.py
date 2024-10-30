0d756fef9354494522f0e3bc16f81d02f207c969
NatesHonor <nathaniel.j.marcellus@gmail.com>
2024-10-30 15:53:28 -0400

    Added pumpkin gathering logic

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import os
 import configparser
 import logging
 from concurrent.futures import ThreadPoolExecutor
-
 from selenium.webdriver.common.by import By
-from threading import Lock
+from threading import Lock, Thread
 from multiprocessing import Value
-
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from data.vehicle_mapping import vehicle_map
 from data.personnel_mapping import personnel_map
 from functions import gather_missions, setup_driver
-
 from scripts.gather_missions import total_number_of_missions
 from scripts.dispatch.dispatcher import dispatch_vehicles
+from scripts.logon import login
 from scripts.vehicle_data import gather_vehicle_data
 from utils.tables.maintables import display_final_table, display_missions_data, display_mission_table
 from utils.version_checker import check_version
@@ -22,42 +22,44 @@ from utils.settings import settings
 from scripts.missions.transport_needed import check_transport_needed
 from utils.website import navigate_to_url
 
-
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name=s - %(levelname=s - %(message=s',
-    handlers=[
-        logging.FileHandler("app.log"),
-        logging.StreamHandler()
-    ]
-)
-
-logger = logging.getLogger(__name__)
-
 check_version()
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 threads = config.getint('client', 'threads', fallback=1)
-print("Logging into threads")
-
 logging.info("Applying settings!")
 settings()
+
+def check_missions_for_pumpkins():
+    driver = login()
+    driver.get("https://www.missionchief.com/")
+    total_pumpkins_found = 0
+    while True:
+        try:
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'missions')))
+            mission_list = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mission_list')))
+            mission_panels = mission_list.find_elements(By.CSS_SELECTOR, 'div.mission_panel')
+            for mission in mission_panels:
+                try:
+                    pumpkin_link = mission.find_element(By.CSS_SELECTOR, 'a#easter-egg-link')
+                    pumpkin_link.click()
+                    total_pumpkins_found += 1
+                except:
+                    continue
+            print(f"Total pumpkins found: {total_pumpkins_found}")
+        except Exception as e:
+            print("Error processing missions:", e)
+        time.sleep(120)
 
 def dispatch_all_missions(driver):
     vehicle_dispatch_mapping = vehicle_map
     personnel_dispatch_mapping = personnel_map
-
     with open('data/vehicle_data.json', 'r') as vehicle_file:
         vehicle_pool = json.load(vehicle_file)
-
     for m_number, mission_info in shared_missions_data.items():
         display_mission_table(shared_missions_data, m_number)
-
-        missionsleep = config.getboolean('missions', 'should_wait_before_missions', fallback=False)
-        missionsleeptime = config.getint('missions', 'should_wait_before_missions_time', fallback=False)
-        if missionsleep:
-            time.sleep(missionsleeptime)
+        if config.getboolean('missions', 'should_wait_before_missions', fallback=False):
+            time.sleep(config.getint('missions', 'should_wait_before_missions_time', fallback=False))
         check_transport_needed(driver)
         try:
             mission_requirements = mission_info['vehicles']
@@ -66,7 +68,6 @@ def dispatch_all_missions(driver):
             dispatch_vehicles(driver, m_number, vehicle_pool, mission_requirements,
                               vehicle_dispatch_mapping, crashed_cars, 'data/missions_data.json',
                               prisoners, personnel_dispatch_mapping)
-
         except KeyError as e:
             logging.error(f"Error processing mission {m_number}: {e}")
         except TypeError as e:
@@ -78,8 +79,10 @@ if __name__ == "__main__":
     shared_missions_data = {}
     shared_lock = Lock()
     total_vehicles_fetched = Value('i', 0)
-
     drivers = [setup_driver() for _ in range(threads)]
+
+    pumpkin_thread = Thread(target=check_missions_for_pumpkins)
+    pumpkin_thread.start()
 
     if not os.path.exists('data/vehicle_data.json'):
         with ThreadPoolExecutor(max_workers=threads) as executor:
@@ -94,8 +97,6 @@ if __name__ == "__main__":
                 driver_vehicle_urls[i % threads].append(vehicle_url)
 
             total_vehicles = len(vehicle_urls)
-            print(f"All threads: Total vehicles found {total_vehicles}")
-
             futures = [executor.submit(gather_vehicle_data, driver, urls, thread_id, shared_vehicle_data, shared_lock, total_vehicles_fetched, total_vehicles)
                        for thread_id, (driver, urls) in enumerate(zip(drivers, driver_vehicle_urls))]
 
@@ -110,10 +111,8 @@ if __name__ == "__main__":
         shared_missions_data.clear()
         first_driver = drivers[0]
         total_missions = total_number_of_missions(first_driver)
-        total_missions_count = len(total_missions)
-        missions_per_thread = total_missions_count // threads
-        extra_missions = total_missions_count % threads
-
+        missions_per_thread = len(total_missions) // threads
+        extra_missions = len(total_missions) % threads
         with ThreadPoolExecutor(max_workers=threads) as executor:
             futures = []
             start = 0
@@ -122,10 +121,8 @@ if __name__ == "__main__":
                 mission_numbers = total_missions[start:end]
                 futures.append(executor.submit(gather_missions, driver, thread_id, mission_numbers, shared_missions_data, shared_lock))
                 start = end
-
             for future in futures:
                 future.result()
-
         with shared_lock:
             with open('data/missions_data.json', 'w') as missions_file:
                 json.dump(shared_missions_data, missions_file)
